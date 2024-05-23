c218d5adc0956b6cc15f3b6b16b855adcc50168a
DarshanSonavane <darshansonavane24@gmail.com>
2024-05-24 01:56:01 +0530

    Changed base URL pointed to server

--- src/app/service/app-services.ts
+++ src/app/service/app-services.ts
@@ -3,8 +3,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable() 
 export class AppServices{
-    BASE_URL = "http://localhost:3000";
-    // BASE_URL = "http://16.170.250.91:3000";
+    // BASE_URL = "http://localhost:3000";
+    BASE_URL = "http://16.170.250.91:3000";
     constructor(private http : HttpClient){}
 
     getAllComplaints = (userDetails : any) => {
